d821fa3646e8a48a185667118f39727730d06bca
Irina Shumak <spunovaiv@rambler.ru>
2022-10-30 12:49:53 +0300

    fix: cors

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const AuthorizationError = require('../errors/authorization-err');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
